a71f27defdd34dc7c74029c21d0d593eae043382
Арина <skorobogatova.ari@yandex.ru>
2025-05-04 15:50:47 +0300

    poyasn zapiska

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from telebot import types
 import telebot
 from parser import *
 from db import *
+from requests import get
 
-bot = telebot.TeleBot('7833937382:AAEY_5GgGZRGWNCvvPWjcgFB48KygWHXhgs')
+bot = telebot.TeleBot('')
 ADMIN = ''
 
 zodiac_signs = {
@@ -26,7 +27,8 @@ zodiac_signs = {
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)  # Здесь изменено значение параметра
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                       one_time_keyboard=False)  # Здесь изменено значение параметра
     col1, col2 = [], []
     for sign in zodiac_signs:
         if len(col1) < 6:
@@ -35,17 +37,79 @@ def send_welcome(message):
             col2.append(sign)
     markup.add(*col1)
     markup.add(*col2)
-    wlcmmsg = '<b>👋 Привет '  + message.from_user.first_name  + '</b>' + '\n\n⚛️ Выберите Ваш знак зодиака'
-    bot.send_message(message.from_user.id, text=wlcmmsg, reply_markup=markup, parse_mode="html", disable_web_page_preview=True)
+    wlcmmsg = '<b>👋 Привет ' + message.from_user.first_name + '</b>' + '\n\n⚛️ Выберите Ваш знак зодиака'
+    bot.send_message(message.from_user.id, text=wlcmmsg, reply_markup=markup, parse_mode="html",
+                     disable_web_page_preview=True)
     tgidregister(message.from_user.id)
+
 
 @bot.message_handler(content_types=['text'])
 def process_step(message):
-    sign = zodiac_signs.get(message.text)
-    if sign:
-        keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(*[InlineKeyboardButton(text=period, callback_data=f'{sign}|{period}') for period in ['вчера', 'сегодня', 'завтра', 'неделя', 'месяц', 'год']])
-        bot.send_message(message.from_user.id, f'Получить гороскоп {message.text} на:', reply_markup=keyboard, parse_mode="html")
+    k = a = message.text
+    a = a.split('.')
+    if len(a) == 2:
+        a = [int(i) for i in a]
+        if ((a[0] in [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31] and a[1] == 3) or
+                (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19] and a[1] == 4)):
+            bot.reply_to(message, 'Вы ♈️ Овен')
+        elif ((a[0] in [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30] and a[1] == 4) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] and a[1] == 5)):
+            bot.reply_to(message, 'Вы ♉ Телец')
+        elif ((a[0] in [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31] and a[1] == 5) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] and a[1] == 6)):
+            bot.reply_to(message, 'Вы ♊ Близнецы')
+        elif ((a[0] in [21, 22, 23, 24, 25, 26, 27, 28, 29, 30] and a[1] == 6) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22] and a[1] == 7)):
+            bot.reply_to(message, 'Вы ♋️ Рак')
+        elif ((a[0] in [23, 24, 25, 26, 27, 28, 29, 30, 31] and a[1] == 7) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22] and a[1] == 8)):
+            bot.reply_to(message, 'Вы ♌ Лев')
+        elif ((a[0] in [23, 24, 25, 26, 27, 28, 29, 30, 31] and a[1] == 8) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22] and a[1] == 9)):
+            bot.reply_to(message, 'Вы ♍ Дева')
+        elif ((a[0] in [23, 24, 25, 26, 27, 28, 29, 30] and a[1] == 9) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22] and a[1] == 10)):
+            bot.reply_to(message, 'Вы ♎ Весы')
+        elif ((a[0] in [23, 24, 25, 26, 27, 28, 29, 30, 31] and a[1] == 10) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21] and a[1] == 11)):
+            bot.reply_to(message, 'Вы ♏ Скорпион')
+        elif ((a[0] in [22, 23, 24, 25, 26, 27, 28, 29, 30] and a[1] == 11) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21] and a[1] == 12)):
+            bot.reply_to(message, 'Вы ♐ Стрелец')
+        elif ((a[0] in [22, 23, 24, 25, 26, 27, 28, 29, 30, 31] and a[1] == 12) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19] and a[1] == 1)):
+            bot.reply_to(message, 'Вы ♑ Козерог')
+        elif ((a[0] in [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31] and a[1] == 1) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] and a[1] == 2)):
+            bot.reply_to(message, 'Вы ♒ Водолей')
+        elif ((a[0] in [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29] and a[1] == 2) or
+              (a[0] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] and a[1] == 3)):
+            bot.reply_to(message, 'Вы ♓ Рыбы')
+
+    else:
+        sign = zodiac_signs.get(message.text)
+        if sign:
+            ph = {
+                '♈️ Овен': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/oven.jpg',
+                '♉ Телец': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/telec.jpg',
+                '♊ Близнецы': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/bliznecy.jpg',
+                '♋️ Рак': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/rak.jpg',
+                '♌ Лев': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/lev.jpg',
+                '♍ Дева': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/deva.jpg',
+                '♎ Весы': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/vesy.jpg',
+                '♏ Скорпион': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/skorpion.jpg',
+                '♐ Стрелец': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/strelec.jpg',
+                '♑ Козерог': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/kozerog.jpg',
+                '♒ Водолей': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/vodolej.jpg',
+                '♓ Рыбы': 'https://cdn.fishki.net/upload/post/2019/08/20/3063203/tn/ryby.jpg'
+            }
+            keyboard = types.InlineKeyboardMarkup()
+            keyboard.add(*[InlineKeyboardButton(text=period, callback_data=f'{sign}|{period}') for period in
+                           ['вчера', 'сегодня', 'завтра', 'неделя', 'месяц', 'год']])
+            bot.send_photo(message.from_user.id, get(ph[message.text]).content)
+            bot.send_message(message.from_user.id, f'Получить гороскоп {message.text} на:', reply_markup=keyboard,
+                             parse_mode="html")
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -58,6 +122,16 @@ def callback_worker(call):
         'месяц': 'month',
         'год': 'year'
     }
-    bot.send_message(call.message.chat.id, getHoro(el[0], period_text[el[1]]), parse_mode="html", disable_web_page_preview=True)
+    bot.send_message(call.message.chat.id, getHoro(el[0], period_text[el[1]]), parse_mode="html",
+                     disable_web_page_preview=True)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Близнецы?')
+def how_are_you(message: types.Message):
+    bot.send_message(
+        message.from_user.id,
+        "кушаю, а ты?",
+    )
+
 
 bot.polling(none_stop=True, interval=0)
